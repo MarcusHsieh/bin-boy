c46b5f921c6176fe8ccd1e363e33df9b4fa8accd
MarcusHsieh <marcus.j.hsieh@gmail.com>
2025-04-19 17:29:04 -0700

    Adjust publish rate

--- csi_camera_ros2/csi_camera_ros2/csi_camera_node.py
+++ csi_camera_ros2/csi_camera_ros2/csi_camera_node.py
@@ -28,9 +28,9 @@ class CSICameraNode(Node):
         self.declare_parameter('sensor_mode', 0) # Using mode 0, but requesting lower resolution output
         self.declare_parameter('capture_width', 640)  # Default lowered further (VGA)
         self.declare_parameter('capture_height', 480) # Default lowered further (VGA)
-        self.declare_parameter('framerate', 30)       # Default framerate 30fps
-        self.declare_parameter('publish_rate', 30.0)  # Match default framerate
-        self.declare_parameter('publish_compressed', True) # Add parameter to control compressed publishing
+        self.declare_parameter('framerate', 30)       # Camera capture framerate
+        self.declare_parameter('publish_rate', 15.0)  # Lower default publish rate for smoother viewing
+        self.declare_parameter('publish_compressed', True) # Publish compressed by default
 
         # Get parameters
         self.sensor_id = self.get_parameter('sensor_id').get_parameter_value().integer_value
